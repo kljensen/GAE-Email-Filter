5dc34decc2ac31c610dd12a5392de6bf12eee343
Kyle L. Jensen <kljensen@gmail.com>
2010-04-28 17:46:27 -0700

    Showing some of my personal settings

--- settings.py
+++ settings.py
@@ -2,3 +2,16 @@ from mail_processor import *
 import logging
 class your_mail_processor(mail_processor):
     """docstring for your_mail_processor"""
+    address_book = {
+        'post' : 'post@kyle-jensen.posterous.com',
+    }
+    email_aliases = {
+        'kljensen@gmail.com' : 'kljensen@gmail.com',
+        'kyle@pipra.org' : 'kljensen@gmail.com',
+    }
+    filters = {
+        'm' : ('Markdown', do_markdown),
+        # 'g' : ('Google', do_google_for_msg),
+    }
+
+    # default_filters = ['g','m']
